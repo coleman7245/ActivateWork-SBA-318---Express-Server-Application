7b2819ae039b93c0291fb2469334f702c6ac51db
coleman7245 <dlcoleman1@live.com>
2025-01-23 14:36:02 -0700

    Created the basic Express server.

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+const companies = require("./data/companies");
+const genres = require("./data/genres");
+const titles = require("./data/titles");
+
+const error = require("./error/error.js");
+
+const app = express();
+
+let port = 8080;
+
+app.get("/", (req, res) => {
+    res.send("<h1>Here I am!</h1>");
+});
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+});
--- main/index.js
+++
@@ -1,6 +0,0 @@
-const express = require("express");
-
-const companies = require("./data/companies.js");
-const genres = require("./data/genres.js");
-const titles = require("./data/titles.js");
-
