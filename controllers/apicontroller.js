7d339c2604c5f728b69b60931bad0bfce6832bca
coleman7245 <dlcoleman1@live.com>
2025-01-27 11:44:34 -0700

    Adjusted the view code and added an additional controller.

---
+++ controllers/apicontroller.js
@@ -0,0 +1,51 @@
+function getAPIRequests(req, res) {
+    const links = [
+        {
+            href : "/api",
+            rel : "api",
+            type: "GET"
+        }
+    ];
+
+    //res.json({links});
+    res.render("index");
+}
+
+function getDataRequests(req, res) {
+    const links = [
+        {
+            href : "api/companies",
+            rel : "companies",
+            type: "GET"
+        },
+        {
+            href : "api/companies",
+            rel : "companies",
+            type: "POST"
+        },
+        {
+            href : "api/titles",
+            rel : "titles",
+            type : "GET"
+        },
+        {
+            href : "api/titles",
+            rel : "titles",
+            type : "POST"
+        },
+        {
+            href : "api/genres",
+            rel : "genres",
+            type : "GET"
+        },
+        {
+            href : "api/genres",
+            rel : "genres",
+            type : "POST"
+        }
+    ];
+
+    res.send(links);
+}
+
+module.exports = {getAPIRequests, getDataRequests};
--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
+const {getAPIRequests, getDataRequests} = require("./controllers/apicontroller");
 const companies = require("./routes/companies");
 const genres = require("./routes/genres");
 const titles = require("./routes/titles");
-
 const error = require("./error/error.js");
 
 const app = express();
@@ -13,60 +13,17 @@ let port = 8080;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json({ extended: true }));
 
-app.set("view", "ejs");
+app.use(express.static(__dirname + "/public"));
+app.set("views", "./views");
+app.set("view engine", "ejs");
 
 app.use("/api/companies", companies);
 app.use("/api/titles", titles);
 app.use("/api/genres", genres);
 
-app.get("/", (req, res) => {
-    const links = [
-        {
-            href : "/api",
-            rel : "api",
-            type: "GET"
-        }
-    ];
+app.get("/", getAPIRequests);
 
-    res.json({links});
-});
-
-app.get("/api", (req, res) => {
-    const links = [
-        {
-            href : "api/companies",
-            rel : "companies",
-            type: "GET"
-        },
-        {
-            href : "api/companies",
-            rel : "companies",
-            type: "POST"
-        },
-        {
-            href : "api/titles",
-            rel : "titles",
-            type : "GET"
-        },
-        {
-            href : "api/titles",
-            rel : "titles",
-            type : "POST"
-        },
-        {
-            href : "api/genres",
-            rel : "genres",
-            type : "GET"
-        },
-        {
-            href : "api/genres",
-            rel : "genres",
-            type : "POST"
-        }
-    ];
-
-    res.send(links);
-});
+app.get("/api", getDataRequests);
 
 app.use((req, res, next) => {
     next(error(404, "Resource Not Found"));
